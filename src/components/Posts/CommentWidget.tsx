47794e5d6e668bb8dd3414ff60acd92b5e1d19c1
김태현 <xogus303@github.com>
2022-11-08 22:39:44 +0900

    Initial Commit

---
+++ src/components/Posts/CommentWidget.tsx
@@ -0,0 +1,51 @@
+import React, { createRef, FunctionComponent, useEffect } from 'react'
+import styled from '@emotion/styled'
+
+const src = 'https://utteranc.es/client.js'
+const repo = 'xogus303/xogus303.github.io'
+
+const UtterancesWrapper = styled.div`
+  @media (max-width: 768px) {
+    padding: 0 20px;
+  }
+`
+
+type UtterancesAttributesType = {
+  src: string
+  repo: string
+  'issue-term': string
+  label: string
+  theme: string
+  crossorigin: string
+  async: string
+}
+
+const CommentWidget: FunctionComponent = () => {
+  const element = createRef<HTMLDivElement>()
+
+  useEffect(() => {
+    if (element.current === null) return
+
+    const utterances: HTMLScriptElement = document.createElement('script')
+
+    const attributes: UtterancesAttributesType = {
+      src,
+      repo,
+      'issue-term': 'pathname',
+      label: 'Comment',
+      theme: `github-light`,
+      crossorigin: 'anonymous',
+      async: 'true',
+    }
+
+    Object.entries(attributes).forEach(([key, value]) => {
+      utterances.setAttribute(key, value)
+    })
+
+    element.current.appendChild(utterances)
+  }, [])
+
+  return <UtterancesWrapper ref={element} />
+}
+
+export default CommentWidget
